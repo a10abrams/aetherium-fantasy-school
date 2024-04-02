9c99528f45660e6471b2dcaf837c19e4af7dc98b
a10abrams <108836278+a10abrams@users.noreply.github.com>
2024-04-02 09:43:02 -0400

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 import Link from "next/link";
 import Navbar from "@/components/navigation/navbar";
 import { Lato, IM_Fell_English } from 'next/font/google';
-import Footer from "@/components/navigation/footer";
-import Reveal from "@/components/reveal/reveal";
-import TopButton from "@/components/buttons/to_top";
+import Footer from "../../components/navigation/footer";
+import Reveal from "../../components/reveal/reveal";
+import TopButton from "../../components/buttons/to_top";
 import '../../styles/contact/contact_styles.css';
 
 const english = IM_Fell_English({ weight: '400', subsets: ['latin'] });
