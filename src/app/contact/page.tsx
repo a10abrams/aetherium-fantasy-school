433fbf6298024d2b5085183f442e639c8b770fbf
a10abrams <108836278+a10abrams@users.noreply.github.com>
2024-04-02 10:29:56 -0400

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import Navbar from "@/components/navigation/navbar";
+import Navbar from "../../components/navigation/navbar";
 import { Lato, IM_Fell_English } from 'next/font/google';
 import Footer from "../../components/navigation/footer";
 import Reveal from "../../components/reveal/reveal";
