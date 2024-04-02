dc36c3983ed40c97a5429879a0fb4b2b74bbbb32
a10abrams <108836278+a10abrams@users.noreply.github.com>
2024-04-02 09:29:29 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    font: {
-      urls: [
-        {
-          src: 'https://fonts.googleapis.com/css2?family=IM+Fell+English&display=swap',
-          family: 'IM Fell English',
-          strategy: 'self',
-        },
-        {
-          src: 'https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap',
-          family: 'Lato',
-          strategy: 'self',
-        },
-      ],
-    },
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
+
