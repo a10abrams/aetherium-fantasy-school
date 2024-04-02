d1ef652c5c47038495d411786d4d3445beb9b918
a10abrams <108836278+a10abrams@users.noreply.github.com>
2024-04-02 10:10:24 -0400

    Update page.tsx

--- src/app/footer_docs/nondiscrim/page.tsx
+++ src/app/footer_docs/nondiscrim/page.tsx
@@ -2,10 +2,10 @@
 
 import { SetStateAction, useState } from 'react';
 import Image from 'next/image';
-import Navbar from "@/components/navigation/navbar";
-import Footer from "@/components/navigation/footer";
-import TopButton from "@/components/buttons/to_top";
-import Dropdown from '@/components/reveal/dropdown';
+import Navbar from "../../../components/navigation/navbar";
+import Footer from "../../..//components/navigation/footer";
+import TopButton from "../../..//components/buttons/to_top";
+import Dropdown from '../../..//components/reveal/dropdown';
 import Signatures from '../../../../public/align_prof_signatures.png';
 import '../../../styles/footer_links/nondiscrim_styles.css';
 
